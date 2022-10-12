519264a581be1f6dba2c67aa4f20a7a4ee1c7d41
chna2cub <cubfox3@gmail.com>
2022-10-11 22:48:42 -0700

    Initial commit

---
+++ src/TreeDataArray.js
@@ -0,0 +1,197 @@
+export const treeDataArray = [
+  {
+    key: "Category-A",
+    label: "Category-A",
+    nodetype: "category",
+    nodes: [
+      {
+        key: "Category-A-SubCategory",
+        label: "Category-A-SubCategory",
+        nodetype: "category",
+        nodes: [
+          {
+            key: "Category-A-Subcategory-Forum",
+            label: "Category-A-Subcategory-Forum",
+            nodetype: "forum",
+            nodes: []
+          },
+          {
+            key: "Category-A-Subcategory-Blog",
+            label: "Category-A-Subcategory-Blog",
+            nodetype: "blog",
+            nodes: []
+          },
+          {
+            key: "Category-A-Subcategory-TKB",
+            label: "Category-A-Subcategory-TKB",
+            nodetype: "tkb",
+            nodes: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    key: "GroupHub-A",
+    label: "GroupHub-A",
+    nodetype: "grouphub",
+    nodes: [
+      {
+        key: "GroupHub-A-SubCategory",
+        label: "GroupHub-A-SubCategory",
+        nodetype: "category",
+        nodes: [
+          {
+            key: "GroupHub-A-SubCategory-Forum",
+            label: "GroupHub-A-SubCategory-Forum",
+            nodetype: "forum"
+          },
+          {
+            key: "GroupHub-A-SubCategory-Blog",
+            label: "GroupHub-A-SubCategory-Blog",
+            nodetype: "blog"
+          },
+          {
+            key: "GroupHub-A-SubCategory-TKB",
+            label: "GroupHub-A-SubCategory-TKB",
+            nodetype: "tkb"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    key: "Forum-A",
+    label: "Forum-A",
+    nodetype: "forum"
+  },
+  {
+    key: "Blog-A",
+    label: "Blog-A",
+    nodetype: "blog"
+  },
+  {
+    key: "TKB-A",
+    label: "TKB-A",
+    nodetype: "tkb"
+  },
+  {
+    key: "Category-B",
+    label: "Category-B",
+    nodetype: "category",
+    nodes: [
+      {
+        key: "Category-B-Forum",
+        label: "Category-B-Forum",
+        nodetype: "forum",
+        nodes: []
+      },
+      {
+        key: "Category-B-Blog",
+        label: "Category-B-Blog",
+        nodetype: "blog",
+        nodes: []
+      },
+      {
+        key: "Category-B-TKB",
+        label: "Category-B-TKB",
+        nodetype: "tkb",
+        nodes: []
+      }
+    ]
+  },
+  {
+    key: "Forum-B",
+    label: "Forum-B",
+    nodetype: "forum"
+  },
+  {
+    key: "Blog-B",
+    label: "Blog-B",
+    nodetype: "blog"
+  },
+  {
+    key: "TKB-B",
+    label: "TKB-B",
+    nodetype: "tkb"
+  },
+  {
+    key: "Category-Many-Nodes",
+    label: "Category-Many-Nodes",
+    nodetype: "category",
+    nodes: [
+      {
+        key: "Category-Many-Nodes-Forum-A",
+        label: "Category-Many-Nodes-Forum-A",
+        nodetype: "forum",
+        nodes: []
+      },
+      {
+        key: "Category-Many-Nodes-Blog-A",
+        label: "Category-Many-Nodes-Blog-A",
+        nodetype: "blog",
+        nodes: []
+      },
+      {
+        key: "Category-Many-Nodes-TKB-A",
+        label: "Category-Many-Nodes-TKB-A",
+        nodetype: "tkb",
+        nodes: []
+      },
+      {
+        key: "Category-Many-Nodes-GroupHub-A",
+        label: "Category-Many-Nodes-GroupHub-A",
+        nodetype: "grouphub",
+        nodes: []
+      },
+      {
+        key: "Category-Many-Nodes-Forum-B",
+        label: "Category-Many-Nodes-Forum-B",
+        nodetype: "forum",
+        nodes: []
+      },
+      {
+        key: "Category-Many-Nodes-Blog-B",
+        label: "Category-Many-Nodes-Blog-B",
+        nodetype: "blog",
+        nodes: []
+      },
+      {
+        key: "Category-Many-Nodes-TKB-B",
+        label: "Category-Many-Nodes-TKB-B",
+        nodetype: "tkb",
+        nodes: []
+      },
+      {
+        key: "Category-Many-Nodes-GroupHub-B",
+        label: "Category-Many-Nodes-GroupHub-B",
+        nodetype: "grouphub",
+        nodes: []
+      },
+      {
+        key: "Category-Many-Nodes-Forum-C",
+        label: "Category-Many-Nodes-Forum-C",
+        nodetype: "forum",
+        nodes: []
+      },
+      {
+        key: "Category-Many-Nodes-Blog-C",
+        label: "Category-Many-Nodes-Blog-C",
+        nodetype: "blog",
+        nodes: []
+      },
+      {
+        key: "Category-Many-Nodes-TKB-C",
+        label: "Category-Many-Nodes-TKB-C",
+        nodetype: "tkb",
+        nodes: []
+      },
+      {
+        key: "Category-Many-Nodes-GroupHub-C",
+        label: "Category-Many-Nodes-GroupHub-C",
+        nodetype: "grouphub",
+        nodes: []
+      }
+    ]
+  }
+];
---
+++ src/TreeDataObject.js
@@ -0,0 +1,198 @@
+export const treeDataObject = {
+  Community: {
+    label: "Community",
+    index: 0,
+    nodetype: "category",
+    nodes: {
+      "Category-A": {
+        label: "Category-A",
+        index: 0,
+        nodetype: "category",
+        nodes: {
+          "Category-A-SubCategory": {
+            label: "Category-A-SubCategory",
+            index: 0,
+            nodetype: "category",
+            nodes: {
+              "Category-A-Subcategory-Forum": {
+                label: "Category-A-Subcategory-Forum",
+                index: 0,
+                nodetype: "forum"
+              },
+              "Category-A-Subcategory-Blog": {
+                label: "Category-A-Subcategory-Blog",
+                index: 1,
+                nodetype: "blog"
+              },
+              "Category-A-Subcategory-TKB": {
+                label: "Category-A-Subcategory-TKB",
+                index: 2,
+                nodetype: "tkb"
+              }
+            }
+          }
+        }
+      },
+      "GroupHub-A": {
+        index: 1,
+        label: "GroupHub-A",
+        nodetype: "grouphub",
+        nodes: {
+          "GroupHub-A-SubCategory": {
+            index: 0,
+            label: "GroupHub-A-SubCategory",
+            nodetype: "category",
+            nodes: {
+              "GroupHub-A-SubCategory-Forum": {
+                index: 0,
+                label: "GroupHub-A-SubCategory-Forum",
+                nodetype: "forum"
+              },
+              "GroupHub-A-SubCategory-Blog": {
+                index: 1,
+                label: "GroupHub-A-SubCategory-Blog",
+                nodetype: "blog"
+              },
+              "GroupHub-A-SubCategory-TKB": {
+                index: 2,
+                label: "GroupHub-A-SubCategory-TKB",
+                nodetype: "tkb"
+              }
+            }
+          }
+        }
+      },
+      "Forum-A": {
+        index: 2,
+        label: "Forum-A",
+        nodetype: "forum"
+      },
+      "Blog-A": {
+        index: 3,
+        label: "Blog-A",
+        nodetype: "blog"
+      },
+      "TKB-A": {
+        index: 4,
+        label: "TKB-A",
+        nodetype: "tkb"
+      },
+      "Category-B": {
+        index: 5,
+        label: "Category-B",
+        nodetype: "category",
+        nodes: {
+          "Category-B-Forum": {
+            index: 0,
+            label: "Category-B-Forum",
+            nodetype: "forum"
+          },
+          "Category-B-Blog": {
+            index: 1,
+            label: "Category-B-Blog",
+            nodetype: "blog"
+          },
+          "Category-B-TKB": {
+            index: 2,
+            label: "Category-B-TKB",
+            nodetype: "tkb"
+          }
+        }
+      },
+      "Forum-B": {
+        index: 6,
+        label: "Forum-B",
+        nodetype: "forum"
+      },
+      "Blog-B": {
+        index: 7,
+        label: "Blog-B",
+        nodetype: "blog"
+      },
+      "TKB-B": {
+        index: 8,
+        label: "TKB-B",
+        nodetype: "tkb"
+      },
+      "Category-Many-Nodes": {
+        index: 9,
+        label: "Category-Many-Nodes",
+        nodetype: "category",
+        nodes: {
+          "Category-Many-Nodes-Forum-A": {
+            index: 0,
+            label: "Category-Many-Nodes-Forum-A",
+            nodetype: "forum"
+          },
+          "Category-Many-Nodes-Blog-A": {
+            index: 1,
+            label: "Category-Many-Nodes-Blog-A",
+            nodetype: "blog"
+          },
+          "Category-Many-Nodes-TKB-A": {
+            index: 2,
+            label: "Category-Many-Nodes-TKB-A",
+            nodetype: "tkb"
+          },
+          "Category-Many-Nodes-GroupHub-A": {
+            index: 3,
+            label: "Category-Many-Nodes-GroupHub-A",
+            nodetype: "grouphub",
+            nodes: {
+              "Category-Many-Nodes-GroupHub-A-Forum": {
+                label: "Category-Many-Nodes-GroupHub-A-Forum",
+                nodetype: "forum"
+              }
+            }
+          },
+          "Category-Many-Nodes-Forum-B": {
+            index: 4,
+            label: "Category-Many-Nodes-Forum-B",
+            nodetype: "forum"
+          },
+          "Category-Many-Nodes-Blog-B": {
+            index: 5,
+            label: "Category-Many-Nodes-Blog-B",
+            nodetype: "blog"
+          },
+          "Category-Many-Nodes-TKB-B": {
+            index: 6,
+            label: "Category-Many-Nodes-TKB-B",
+            nodetype: "tkb"
+          },
+          "Category-Many-Nodes-GroupHub-B": {
+            index: 7,
+            label: "Category-Many-Nodes-GroupHub-B",
+            nodetype: "grouphub",
+            nodes: {
+              "Category-Many-Nodes-GroupHub-B-Blog": {
+                label: "Category-Many-Nodes-GroupHub-B-Blog",
+                nodetype: "blog"
+              }
+            }
+          },
+          "Category-Many-Nodes-Forum-C": {
+            index: 8,
+            label: "Category-Many-Nodes-Forum-C",
+            nodetype: "forum"
+          },
+          "Category-Many-Nodes-Blog-C": {
+            index: 9,
+            label: "Category-Many-Nodes-Blog-C",
+            nodetype: "blog"
+          },
+          "Category-Many-Nodes-TKB-C": {
+            index: 10,
+            label: "Category-Many-Nodes-TKB-C",
+            nodetype: "tkb"
+          },
+          "Category-Many-Nodes-GroupHub-C": {
+            index: 11,
+            label: "Category-Many-Nodes-GroupHub-C",
+            nodetype: "grouphub"
+          }
+        }
+      }
+    }
+  }
+};
